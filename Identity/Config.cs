44fe32aff76e2556939e2aec5d22d964d8549463
Cubelaster <david.cubela@gmail.com>
2022-11-08 07:08:53 +0100

    Added initial config for Identity and verified it works on React client

--- Identity/Config.cs
+++ Identity/Config.cs
@@ -1,4 +1,6 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using IdentityModel;
 
 namespace Identity
 {
@@ -7,32 +9,57 @@ namespace Identity
         public static IEnumerable<IdentityResource> IdentityResources =>
             new IdentityResource[]
             {
-                new IdentityResources.OpenId()
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResource
+                {
+                    Name = "verification",
+                    DisplayName = "Verification",
+                    Description = "Claims used for verification",
+                    UserClaims = new List<string>
+                    {
+                        JwtClaimTypes.Email,
+                        JwtClaimTypes.EmailVerified
+                    }
+                },
+                new IdentityResource("color", new [] { "favorite_color" })
             };
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new List<ApiScope> {
-                new ApiScope(name: "api1", displayName: "MyAPI")
+                new ApiScope
+                {
+                    Name = "testscope",
+                    DisplayName = "Test Scope",
+                    Description = "Scope used for testing purposes"
+                }
             };
 
         public static IEnumerable<Client> Clients =>
             new List<Client>
             {
+                // React Client
                 new Client
                 {
-                    ClientId = "client",
+                    ClientId = "react",
+                    ClientName = "React Client",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false,
 
-                    // no interactive user, use the clientid/secret for authentication
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    RedirectUris =           { "https://localhost:5005/callback" },
+                    PostLogoutRedirectUris = { "https://localhost:5005/" },
+                    AllowedCorsOrigins =     { "https://localhost:5005" },
 
-                    // secret for authentication
-                    ClientSecrets =
+                    AllowedScopes = new List<string>
                     {
-                        new Secret("secret".Sha256())
-                    },
-
-                    // scopes that client has access to
-                    AllowedScopes = { "api1" }
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "verification",
+                        "testscope",
+                        "color"
+                    }
                 }
             };
     }
--- Identity/HostingExtensions.cs
+++ Identity/HostingExtensions.cs
@@ -7,7 +7,7 @@ namespace Identity
         public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
         {
             // uncomment if you want to add a UI
-            //builder.Services.AddRazorPages();
+            builder.Services.AddRazorPages();
 
             builder.Services.AddIdentityServer(options =>
                 {
@@ -16,7 +16,8 @@ namespace Identity
                 })
                 .AddInMemoryIdentityResources(Config.IdentityResources)
                 .AddInMemoryApiScopes(Config.ApiScopes)
-                .AddInMemoryClients(Config.Clients);
+                .AddInMemoryClients(Config.Clients)
+                .AddTestUsers(TestUsers.Users);
 
             return builder.Build();
         }
@@ -31,14 +32,14 @@ namespace Identity
             }
 
             // uncomment if you want to add a UI
-            //app.UseStaticFiles();
-            //app.UseRouting();
+            app.UseStaticFiles();
+            app.UseRouting();
 
             app.UseIdentityServer();
 
             // uncomment if you want to add a UI
-            //app.UseAuthorization();
-            //app.MapRazorPages().RequireAuthorization();
+            app.UseAuthorization();
+            app.MapRazorPages().RequireAuthorization();
 
             return app;
         }
